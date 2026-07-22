b631ce650105307bc45f4c3d8637112220d06e32
minhcao-creator <minhcao2612@gmail.com>
2026-07-22 10:05:46 +0700

    gitlailai

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import RightSidebar from '../Components/RightSidebar'
 import ContactUs from './ContactUs'
 import AboutMe from './AboutMe'
 import { Routes, Route } from 'react-router-dom'
@@ -9,7 +8,6 @@ import FlipBook from './FlipBook'
 const Home = () => {
   return (
     <div>
-      {/* <RightSidebar/> */}
       <Routes>
         <Route path='/' element={<FlipBook />} />
         <Route path='/contact' element={<ContactUs />} />
